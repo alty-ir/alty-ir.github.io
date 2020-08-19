f2a9c5d6d4a77a1ef98740a667798a55d826f1d1
Ali <ali.tabesh76@gmail.com>
2020-08-19 11:15:03 +0430

    load problem fix

--- _nuxt/static/1597503173/landing/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1597503173",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{navbar:b,i18n:{routeParams:{}}},serverRendered:b,routePath:a,config:{}}}("\u002Flanding",false));
---
+++ _nuxt/static/1597819325/fa/profile/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1597819325",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{navbar:b,i18n:{routeParams:{}}},serverRendered:b,routePath:a,config:{}}}("\u002Ffa\u002Fprofile",false));
